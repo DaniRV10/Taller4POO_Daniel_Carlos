cb1076163f2056818c21c9c87c16207c0ff46d96
Akr0yy <carlos.a.montenegro.p@gmail.com>
2026-07-02 18:32:21 -0400

    test

--- src/logica/App.java
+++ src/logica/App.java
@@ -1,6 +1,9 @@
 package logica;
 
 public class App {
+	
+	//Carlos Alberto Montenegro Perez 22154893-0 ICCI
+	//Daniel Alexanders Robles Valdenegro 20738244-2 ICCI
 
 	public static void main(String[] args) {
 		
